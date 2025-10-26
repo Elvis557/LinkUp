6819e0fdd87dc26d66488eef25c8ac8842c8283c
Elvis Boateng <elvisboateng557@gmail.com>
2025-10-25 12:24:24 -1200

    updated  server.js and index.html

--- server.js
+++ server.js
@@ -1,91 +1,52 @@
-// Import dependencies
 var express = require("express");
 var app = express();
-var http = require("http");
-var server = http.createServer(app);
+var server = require("http").createServer(app);
 var io = require("socket.io")(server);
-var path = require("path");
 
-let users = [];
-let connections = [];
+users = [];
+connections = [];
 
-// ✅ Use Render's assigned port (or 3001 locally)
-const PORT = process.env.PORT || 3001;
+server.listen(3001);
+console.log("Server running on port 3001");
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/", function(req, resp){
+    resp.sendFile(__dirname + "/index.html");
 });
 
-// ✅ Serve static files (like index.html, CSS, JS)
-app.use(express.static(path.join(__dirname)));
-
-// ✅ Serve index.html for root route
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// ✅ Handle Socket.IO connections
-io.on("connection", function (socket) {
-  connections.push(socket);
-  console.log("Connected: %s sockets connected", connections.length);
-
-  // When a new user connects
-  socket.on("new user", function (data, callback) {
-    callback(true);
-    socket.username = data;
-    users.push(socket.username);
-    updateUsernames();
-
-    // Broadcast user joined notification
-    socket.broadcast.emit("user joined", {
-      user: socket.username,
-      timestamp: new Date().toLocaleTimeString(),
+io.sockets.on("connection", function(socket){
+    connections.push(socket);
+    console.log("Connected: %s sockets connected", connections.length);
+    
+    // When a new user connects and sends their username
+    socket.on("new user", function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+        console.log("User joined: " + socket.username);
     });
-
-    console.log("User joined: " + socket.username);
-  });
-
-  // When user disconnects
-  socket.on("disconnect", function () {
-    if (!socket.username) return;
-
-    socket.broadcast.emit("user left", {
-      user: socket.username,
-      timestamp: new Date().toLocaleTimeString(),
+    
+    // When user disconnects
+    socket.on("disconnect", function(data){
+        if(!socket.username) return;
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Disconnected: %s sockets connected", connections.length);
     });
-
-    users.splice(users.indexOf(socket.username), 1);
-    updateUsernames();
-    connections.splice(connections.indexOf(socket), 1);
-    console.log("User left: " + socket.username);
-    console.log("Disconnected: %s sockets connected", connections.length);
-  });
-
-  // When server receives a message
-  socket.on("send message", function (data) {
-    console.log("Message from " + socket.username + ": " + data);
-
-    io.sockets.emit("new message", {
-      msg: data,
-      user: socket.username,
-      timestamp: new Date().toLocaleTimeString(),
+    
+    // When server receives a message
+    socket.on("send message", function(data){
+        console.log("Message from " + socket.username + ": " + data);
+        // Send message to ALL clients including sender
+        io.sockets.emit("new message", {
+            msg: data, 
+            user: socket.username
+        });
     });
-  });
-
-  // Typing indicators
-  socket.on("typing", function () {
-    socket.broadcast.emit("typing", { user: socket.username });
-  });
-
-  socket.on("stop typing", function () {
-    socket.broadcast.emit("stop typing", { user: socket.username });
-  });
-
-  socket.on("message read", function () {
-    socket.broadcast.emit("message read", { user: socket.username });
-  });
-
-  function updateUsernames() {
-    io.sockets.emit("get users", users);
-  }
+    
+    // Update online users for all clients
+    function updateUsernames(){
+        io.sockets.emit("get users", users);
+    }
 });
